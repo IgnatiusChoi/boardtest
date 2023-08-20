eb7e6bfe73e31cc4daaa97b06b2506ec14624330
Ignatius Choi <chldu63@naver.com>
2023-08-20 20:14:37 +0900

    first commit

--- build/generated/sources/annotationProcessor/java/main/board/board/mapstruct/BoardResMapStructImpl.java
+++ build/generated/sources/annotationProcessor/java/main/board/board/mapstruct/BoardResMapStructImpl.java
@@ -13,8 +13,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-08-11T12:06:14+0900",
-    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.3.1.jar, environment: Java 11.0.18 (Oracle Corporation)"
+    date = "2023-08-20T16:43:47+0900",
+    comments = "version: 1.5.5.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.3.1.jar, environment: Java 11.0.16.1 (Oracle Corporation)"
 )
 @Component
 public class BoardResMapStructImpl implements BoardResMapStruct {
